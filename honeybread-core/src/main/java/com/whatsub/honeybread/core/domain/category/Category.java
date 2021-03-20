1024d3ebaf8bf17cf4a11126feacd562d5bcf531
ncucu <ncucu.me@kakaocommerce.com>
2021-03-21 00:13:43 +0900

    [DEVELOP] Category 엔티티 설계

---
+++ honeybread-core/src/main/java/com/whatsub/honeybread/core/domain/category/Category.java
@@ -0,0 +1,22 @@
+package com.whatsub.honeybread.core.domain.category;
+
+import com.whatsub.honeybread.core.domain.base.BaseEntity;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(callSuper = true)
+public class Category extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+}
---
+++ honeybread-core/src/main/java/com/whatsub/honeybread/core/domain/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.whatsub.honeybread.core.domain.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
