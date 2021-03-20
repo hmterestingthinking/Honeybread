ef6ef16141af198124a25f2808c48eb333c3ff04
ncucu <ncucu.me@kakaocommerce.com>
2021-03-21 00:02:10 +0900

    [COMMON] MoneyConverter 추가

---
+++ honeybread-core/src/main/java/com/whatsub/honeybread/core/domain/infra/MoneyConverter.java
@@ -0,0 +1,19 @@
+package com.whatsub.honeybread.core.domain.infra;
+
+import com.whatsub.honeybread.core.domain.model.Money;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class MoneyConverter implements AttributeConverter<Money, Long> {
+    @Override
+    public Long convertToDatabaseColumn(Money money) {
+        return money.getValue().longValue();
+    }
+
+    @Override
+    public Money convertToEntityAttribute(Long value) {
+        return Money.wons(value);
+    }
+}
