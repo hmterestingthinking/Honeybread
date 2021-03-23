364fca69d6e8de8fad9d38ff073ebbddefac017d
sup2is <dev.sup2is@gmail.com>
2021-03-24 00:17:05 +0900

    [Develop] PasswordEncoder 생성

---
+++ honeybread-core/src/main/java/com/whatsub/honeybread/core/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.whatsub.honeybread.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
