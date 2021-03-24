f474cf7fbcecc216d81e4449514e50fb5b602a5c
wanda.777 <wanda.777@kakaocommerce.com>
2021-03-24 19:02:24 +0900

    [ Refactor ] 찜정보 - test code 필요없는 어노테이션 제거

--- honeybread-core/src/test/java/com/whatsub/honeybread/core/domain/userstorefavorite/UserStoreFavoriteRepositoryTest.java
+++ honeybread-core/src/test/java/com/whatsub/honeybread/core/domain/userstorefavorite/UserStoreFavoriteRepositoryTest.java
@@ -1,14 +1,11 @@
 package com.whatsub.honeybread.core.domain.userstorefavorite;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class UserStoreFavoriteRepositoryTest {
 
