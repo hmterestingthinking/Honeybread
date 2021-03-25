ad79c6cb6104b0fa5bf120f2a2417ff811a49da7
ncucu <ncucu.me@kakaocommerce.com>
2021-03-26 01:56:06 +0900

    [ DEVELOP ] Swagger Tag 추가

--- honeybread-mgmtadmin/src/main/java/com/whatsub/honeybread/mgmtadmin/support/MgmtAdminSwaggerTags.java
+++ honeybread-mgmtadmin/src/main/java/com/whatsub/honeybread/mgmtadmin/support/MgmtAdminSwaggerTags.java
@@ -2,4 +2,6 @@ package com.whatsub.honeybread.mgmtadmin.support;
 
 public abstract class MgmtAdminSwaggerTags {
     public static final String CATEGORY = "[01] 카테고리";
+
+    public static final String MENU = "[04] 메뉴";
 }
