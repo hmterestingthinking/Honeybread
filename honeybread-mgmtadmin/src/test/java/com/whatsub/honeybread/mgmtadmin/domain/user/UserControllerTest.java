44f3492ea6b8b07f75abd51b936e9c42f743699a
sup2is <dev.sup2is@gmail.com>
2021-03-26 13:38:50 +0900

    [Refactor] User 등록시 중복된 이메일 테스트 추가

--- honeybread-mgmtadmin/src/test/java/com/whatsub/honeybread/mgmtadmin/domain/user/UserControllerTest.java
+++ honeybread-mgmtadmin/src/test/java/com/whatsub/honeybread/mgmtadmin/domain/user/UserControllerTest.java
@@ -75,6 +75,28 @@ class UserControllerTest {
         //then
         resultActions.andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.errors", hasSize(3)));
+    }    
+    
+    @Test
+    void 유저_등록시_중복된_이메일이면_등록_실패() throws Exception {
+        //given
+        final UserRequest userRequest =
+                new UserRequest("test@honeybread.com", "qwer1234!", "010-9999-9999", false, false);
+
+        given(userService.register(userRequest))
+                .willThrow(new HoneyBreadException(ErrorCode.DUPLICATE_USER_EMAIL));
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                post(BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userRequest)))
+                .andDo(print());
+
+        //then
+        resultActions.andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message", is(ErrorCode.DUPLICATE_USER_EMAIL.getMessage())));
     }
 
     @Test
