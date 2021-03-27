69c4d03042eab3f422ad58edbeba66364a77c5a3
wanda.777 <wanda.777@kakaocommerce.com>
2021-03-27 19:59:57 +0900

    [ Refactor ] 스토어 엔티티 설계 - 코드리뷰 반영 스토어 상태 정리

--- honeybread-core/src/main/java/com/whatsub/honeybread/core/domain/store/StoreStatus.java
+++ honeybread-core/src/main/java/com/whatsub/honeybread/core/domain/store/StoreStatus.java
@@ -1,7 +1,16 @@
 package com.whatsub.honeybread.core.domain.store;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum StoreStatus {
-    OPERATION,
-    TEMP_CLOSURE,
-    WITHDRAW
+    WAITING("입점대기"),
+    REJECTED("입점반려"),
+    ACTIVATED("정상"),
+    DEACTIVATED("비활성"),
+    WITHDRAW("탈퇴");
+
+    private final String description;
 }
